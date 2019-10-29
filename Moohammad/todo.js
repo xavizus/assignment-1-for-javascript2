92dfbe19bf769e6f085fd15eb8e87fe932468a8c
Moohammad Samaae <m.samaae@hotmail.com>
2019-10-29 10:44:23 +0100

    added a toggle all todos method and some html buttons

--- Moohammad/todo.js
+++ Moohammad/todo.js
@@ -2,25 +2,65 @@ let todoList = {
     todos: [],
 
     displayTodos() {
-        console.log("My Todos: ", this.todos);
+        if(this.todos.length === 0) {
+            console.log("Your array is empty!");
+        } else {
+            console.log("My Todos:");       
+            for(let i = 0; i < this.todos.length; i++) {
+                if(this.todos[i].completed === true) {
+                console.log("(x)", this.todos[i].todoText); 
+                } else {
+                    console.log("( )", this.todos[i].todoText); 
+                }
+            }
+        }
     },
-    addTodo(todo) {
-        this.todos.push(todo);
+    addTodo(todoText) {
+        this.todos.push({
+            todoText: todoText,
+            completed: false
+        });
         this.displayTodos();
     },
-    changeTodo(position, newValue) {
-        this.todos[position] = newValue;
+    changeTodo(position, todoText) {
+        this.todos[position].todoText = todoText;
         this.displayTodos();
     },
     deleteTodo(position) {
         this.todos.splice(position, 1);
         this.displayTodos();
-    }
-}
+    },
+    toggleCompleted(position) {
+        let todo = this.todos[position];
+        todo.completed = !todo.completed;
+        this.displayTodos();
+    },
+    toggleAll() {
+        totalTodos = this.todos.length;
+        let completedTodos = 0;
+
+        for(let i = 0; i < totalTodos; i++) {
+            if(this.todos[i].completed === true) {
+                completedTodos++;                
+            }
+        }
+
+        if(completedTodos === totalTodos) {
+            for(let i = 0; i < totalTodos; i++) {
+                this.todos[i].completed = false;
+            }
+        } else {
+            for(let i = 0; i < totalTodos; i++) {
+                this.todos[i].completed = true;
+            }
+        }
+        this.displayTodos();
+    }   
+};
     
 
 
 
-todoList.addTodo("item", "item2");
-todoList.changeTodo(0, "changed item");
-todoList.deleteTodo(0);
+todoList.addTodo("item");
+todoList.addTodo("item2");
+todoList.toggleAll();
