c82d5de53725684019b86b8689057576893ad1d0
Moohammad Samaae <m.samaae@hotmail.com>
2019-11-03 23:14:17 +0100

    moved files

--- application/Moohammad/newtodo.js
+++ application/Moohammad/newtodo.js
@@ -1,48 +1,32 @@
 document.addEventListener("DOMContentLoaded", function(event){
 
-//Get HTML elements
-//let todoUl = document.querySelector("ul");
+
 let input = document.getElementById("input");
 let addBtn = document.getElementById("addBtn");
-let removeBtn = document.getElementById("removeBtn")
 
 addBtn.addEventListener("click", function(e) {
     let item = input.value;
-   /*  let li = document.createElement("li");
-    li.innerHTML = item; */
 
     let checkbox = `
-        <input type="checkbox" class="chkbox">
-        <span class="item">${item}</span>
-        </input>
-        <br>
+        <div class="checkboxDiv"> 
+            <input class="checkbox" type="checkbox"> 
+            <span class="unchecked">${item}</span> 
+        </div>
     `
     ;
 
     document.getElementById("todo-container").insertAdjacentHTML("beforeend", checkbox);
 
-    /* todoUl.appendChild(li);
-    li.appendChild(checkbox);
-     */
 }); 
 
 
 
 
-removeBtn.addEventListener("click", function(e){
-    
+document.addEventListener("click", function(e) { 
+    if(e.target.parentNode.className == "checkboxDiv") { 
+        let item = e.target.parentNode.getElementsByTagName("span")[0]; 
+        item.className = "checked"; 
+    } 
 });
 
-document.getElementById("todo-container").addEventListener("change", (event) => {
-    if(event.target.className == "chkbox") {
-        let item = document.getElementsByClassName("item");
-item.setAttribute("class", "checkedItem");
-
-    }
-});
-
-
-/* 
-document.getElementsByClassName("chkbox").addEventListener('change', function() {
-     if(this.checked) { */
 });
